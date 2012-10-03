7822804df81132311e3223814581a9376cc0b058
Divya Ranjit <divroxalwz@gmail.com>
2012-10-03 03:22:47 -0400

    PushSum Basic Features

--- Project2.scala
+++ Project2.scala
@@ -14,6 +14,7 @@ import scala.Array._
  */
 case object Rumour
 case object TransmitData
+case class PushSum(s: Int, w: Int)
 
 object Project2 {
 
@@ -35,27 +36,35 @@ object Project2 {
   }
 
   def start_process() {
+    var is_pnode = false
+    if (algorithm == "PushSum" || algorithm == "pushsum"|| algorithm == "Pushsum")
+      is_pnode = true
     topology match {
-
-      case "Full" => {
+      case "FULL" | "full" | "Full" => {
         var full_topology = new FullTopology(num_nodes)
-        full_topology.initiate_nodes()
-        full_topology.g_nodes(0) ! Rumour
+        full_topology.initiate_nodes(is_pnode)
+        if(is_pnode)
+          full_topology.g_nodes(0) ! PushSum(0,0)
+        else
+          full_topology.g_nodes(0) ! Rumour
       }
 
-      case "Line" => {
+      case "line" | "LINE" | "Line" => {
         var line_topology = new LineTopology(num_nodes)
-        line_topology.initiate_nodes()
-        line_topology.g_nodes(0) ! Rumour
+        line_topology.initiate_nodes(is_pnode)
+        if(is_pnode)
+          line_topology.g_nodes(0) ! PushSum(0,0)
+        else
+          line_topology.g_nodes(0) ! Rumour
       }
 
-      case "2D" => {
+      case "2D" | "2d" => {
         var twod_topology = new TwoDTopology(num_nodes)
         twod_topology.initiate_nodes()
         twod_topology.g_nodes(0)(0) ! Rumour
       }
 
-      case "2DImp" => {
+      case "2DImp" | "2dimp" | "2Dimp" => {
         var twod_topology = new TwoDImpTopology(num_nodes)
         twod_topology.initiate_nodes()
         twod_topology.g_nodes(0)(0) ! Rumour
@@ -76,6 +85,13 @@ abstract class Topology(num_nodes: Int) {
     }
   }
 
+  def should_exit(sw_array:Array[BigDecimal]):Boolean = {
+    if(sw_array(2)-sw_array(0) <= e-10)
+      return true
+    else
+      return false
+  }
+
   def get_random_neighbour(): Actor
 
   def get_random_neighbour(index: Int): Actor
@@ -89,9 +105,13 @@ class FullTopology(num_nodes: Int) extends Topology(num_nodes) {
   var g_nodes: Array[GNodes] = Array()
   val rand = new Random()
 
-  def initiate_nodes() {
+  def initiate_nodes(is_pnode: Boolean) {
     for (i <- 0 to num_nodes - 1) {
       g_nodes :+= new GNodes(i, this, num_nodes)
+      if (is_pnode){
+        g_nodes(i).is_pnode = is_pnode
+        g_nodes(i).s = i
+      }
       g_nodes(i).start()
     }
   }
@@ -116,9 +136,13 @@ class FullTopology(num_nodes: Int) extends Topology(num_nodes) {
 class LineTopology(num_nodes: Int) extends Topology(num_nodes) {
   var g_nodes: Array[GNodes] = Array()
 
-  def initiate_nodes() {
+  def initiate_nodes(is_pnode: Boolean) {
     for (i <- 0 to num_nodes - 1) {
       g_nodes :+= new GNodes(i, this, num_nodes)
+      if (is_pnode){
+        g_nodes(i).is_pnode = is_pnode
+        g_nodes(i).s = i
+      }
       g_nodes(i).start()
     }
   }
@@ -164,6 +188,7 @@ class LineTopology(num_nodes: Int) extends Topology(num_nodes) {
 class TwoDTopology(num_nodes: Int) extends Topology(num_nodes){
   var use_val:Int = find_dimension(num_nodes)
   var g_nodes = ofDim[TwoDGNodes](use_val,use_val)
+  var neighbour_indices: Array[Tuple2[Int, Int]] = Array()
 
   def initiate_nodes() {
     var index:Tuple2[Int,Int] = (0,0)
@@ -177,11 +202,9 @@ class TwoDTopology(num_nodes: Int) extends Topology(num_nodes){
   }
 
   def find_dimension(num_nodes:Int):Int = {
-
     var node = (Math.ceil(Math.sqrt(num_nodes)))
     println("Dimension is : "+ node)
     return (node).toInt
-
   }
 
   override def get_neighbours(index: Tuple2[Int, Int]):Array[Actor] = {
@@ -191,37 +214,59 @@ class TwoDTopology(num_nodes: Int) extends Topology(num_nodes){
     for (i <- 0 to use_val - 1) {
       for (j<- 0 to use_val - 1){
         if (x == 0 || y == 0){
-          if (x!= use_val -1)
+          if (x!= use_val -1){
             neighbours:+= g_nodes(x+1)(y)
-          if (y!=use_val -1)
+            neighbour_indices:+=((x+1),(y))
+          }
+          if (y!=use_val -1){
             neighbours:+= g_nodes(x)(y+1)
-          if (y!=0)
+            neighbour_indices:+=((x),(y+1))
+          }
+          if (y!=0){
             neighbours:+= g_nodes(x)(y-1)
-          if (x!=0)
+            neighbour_indices:+=((x),(y-1))
+          }
+          if (x!=0){
             neighbours:+= g_nodes(x-1)(y)
+            neighbour_indices:+=((x-1),(y))
+          }
         }
 
         else if (x == use_val - 1){
           neighbours:+= g_nodes(x-1)(y)
-          if (y!= use_val -1)
+          neighbour_indices:+=((x-1),(y))
+          if (y!= use_val -1){
             neighbours:+= g_nodes(x)(y+1)
-          if (y!=0)
+            neighbour_indices:+=((x),(y+1))
+          }
+          if (y!=0){
             neighbours:+= g_nodes(x)(y-1)
+            neighbour_indices:+=((x),(y-1))
+          }
         }
 
         else if (y == use_val - 1){
           neighbours:+= g_nodes(x)(y-1)
-          if (x!=use_val - 1)
+          neighbour_indices:+=((x),(y-1))
+          if (x!=use_val - 1){
             neighbours:+= g_nodes(x+1)(y)
-          if (x!=0)
+            neighbour_indices:+=((x+1),(y))
+          }
+          if (x!=0){
             neighbours:+= g_nodes(x-1)(y)
+            neighbour_indices:+=((x-1),(y))
+          }
         }
 
         else{
           neighbours:+= g_nodes(x)(y-1)
+          neighbour_indices:+=((x),(y-1))
           neighbours:+= g_nodes(x)(y+1)
+          neighbour_indices:+=((x),(y+1))
           neighbours:+= g_nodes(x-1)(y)
+          neighbour_indices:+=((x-1),(y))
           neighbours:+= g_nodes(x+1)(y)
+          neighbour_indices:+=((x+1),(y))
         }
 
       }
@@ -232,9 +277,23 @@ class TwoDTopology(num_nodes: Int) extends Topology(num_nodes){
 
   override def get_imperfect_neighbours(index: Tuple2[Int, Int]): Array[Actor] = {
     var neighbours = get_neighbours(index)
+    var possible_neighbours: Array[Actor] = Array()
+
+    for (i <- 0 to use_val - 1) {
+      for (j<- 0 to use_val - 1){
+        var flag = false
+        for (ind <- neighbour_indices){
+          if (i == ind._1 && j == ind._2)
+            flag = true
+        }
+        if(!flag)
+          possible_neighbours:+= g_nodes(i)(j)
+      }}
+
+    neighbours:+= possible_neighbours(new Random().nextInt(1000) % possible_neighbours.size)
+
     return neighbours
   }
-
 
   override def get_random_neighbour(): Actor = {
     g_nodes(0)(0)
@@ -243,8 +302,6 @@ class TwoDTopology(num_nodes: Int) extends Topology(num_nodes){
   override def get_random_neighbour(index: Int): Actor = {
     g_nodes(0)(0)
   }
-
-
 }
 
 class TwoDImpTopology(num_nodes: Int) extends TwoDTopology(num_nodes){}
@@ -253,6 +310,10 @@ class GNodes(index: Int, topology: Topology, gnode_size: Int) extends Actor {
   var status: Boolean = true
   var count = 0
   val max_count = 5
+  var s = 0
+  var w = 1
+  var is_pnode = false
+  var last_three_received: Array[BigDecimal] = Array()
 
   def act() {
     loop {
@@ -275,11 +336,28 @@ class GNodes(index: Int, topology: Topology, gnode_size: Int) extends Actor {
               topology.get_random_neighbour ! Rumour
             else if (topology.isInstanceOf[LineTopology])
               topology.get_random_neighbour(index) ! Rumour
-            else if (topology.isInstanceOf[TwoDTopology])
-              topology.get_random_neighbour(index) ! Rumour
             Thread.sleep(100)
             self ! TransmitData
           }
+        }
+
+        case PushSum(rs: Int, rw: Int) => {
+          s += rs
+          w += rw
+
+          topology.should_exit(last_three_received)
+          self ! TransmitPSData
+        }
+
+        case TransmitPSData => {
+          if (topology.isInstanceOf[FullTopology])
+            topology.get_random_neighbour ! PushSum(s/2, w/2)
+          else if (topology.isInstanceOf[LineTopology])
+            topology.get_random_neighbour(index) ! PushSum(s/2, w/2)
+          s = s/2
+          w = w/2
+          Thread.sleep(100)
+          self ! TransmitPSData
         }
       }
     }
@@ -293,10 +371,12 @@ class TwoDGNodes(index: Tuple2[Int, Int], topology: Topology, gnode_size: Int) e
   val max_count = 5
   var neighbours:Array[Actor] = Array()
   def act() {
+
     if (topology.isInstanceOf[TwoDTopology])
-    neighbours = topology.get_neighbours(index)
+      neighbours = topology.get_neighbours(index)
     else if (topology.isInstanceOf[TwoDImpTopology])
       neighbours = topology.get_imperfect_neighbours(index)
+
     loop {
       react {
         case Rumour => {
@@ -324,5 +404,44 @@ class TwoDGNodes(index: Tuple2[Int, Int], topology: Topology, gnode_size: Int) e
 
   def get_random_neighbour():Actor = {
     neighbours(new Random().nextInt(10) % neighbours.size)
+  }
+}
+
+class PNodes(index: Int, topology: Topology, pnode_size: Int) extends Actor {
+  var status: Boolean = true
+  var count = 0
+  val max_count = 5
+  var s:Int = 0
+  var w:Int = 1
+
+  def act() {
+    loop {
+      react {
+        case Rumour => {
+          count = count + 1
+          println("topology.deactive_nodes.size : "+ topology.deactive_nodes.size)
+          if (count < max_count)
+            self ! TransmitData
+          else {
+            if (status)
+              topology.increment_status(pnode_size)
+            status = false
+          }
+        }
+
+        case TransmitData => {
+          if (count < max_count) {
+            if (topology.isInstanceOf[FullTopology])
+              topology.get_random_neighbour ! Rumour
+            else if (topology.isInstanceOf[LineTopology])
+              topology.get_random_neighbour(index) ! Rumour
+            else if (topology.isInstanceOf[TwoDTopology])
+              topology.get_random_neighbour(index) ! Rumour
+            Thread.sleep(100)
+            self ! TransmitData
+          }
+        }
+      }
+    }
   }
 }
